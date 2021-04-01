4aa9a0d782c85ce590822c380751a48946d5293a
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-01 10:45:44 +0300

    Ensimmäinen versio, joka ainoastaan näyttää kävijämäärät

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    db.session.execute("INSERT INTO visitors (time) VALUES (NOW())")
+    db.session.commit()
+    result = db.session.execute("SELECT COUNT(*) FROM visitors")
+    counter = result.fetchone()[0]
+    return render_template("index.html", counter=counter) 
