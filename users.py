d89a116526e1344c7a74eba2519d294ac0a1c8dc
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-07 20:19:11 +0300

    virheen korjaamista esimerkistä

--- users.py
+++ users.py
@@ -1,3 +1,7 @@
+from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
 def login(username,password):
     sql = "SELECT password, id FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
