4239cc42eb8eaa19a4ad426197cbfde28d0582ed
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-07 20:13:15 +0300

    virheen etsimistä

--- messages.py
+++ messages.py
@@ -3,11 +3,9 @@ from db import db
 import users
 
 def get_list():
-    sql = "SELECT M.content, U.username, M.sent_at FROM messages M, users U" \
-          "WHERE M.user_id=U.id ORDER BY M.id"
+    sql = "SELECT M.content, U.username, M.sent_at FROM messages M, users U WHERE M.user_id=U.id ORDER BY M.id"
     result = db.session.execute(sql)
     return result.fetchall()
-
 
 def send(content):
     user_id = users.user_id()
