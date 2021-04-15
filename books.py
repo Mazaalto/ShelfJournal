86d2d6cce1bfd6863b9856b36d9c36c65dc2db30
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-15 09:37:59 +0300

    Testing

--- books.py
+++ books.py
@@ -37,7 +37,7 @@ def search_from_genre(query):
     return result.fetchall()
 
 def get_book(id):
-    sql = "SELECT book_title, author_name, user_id, sent_at, info FROM books WHERE id=:id"
+    sql = "SELECT id, book_title, author_name, user_id, sent_at, info FROM books WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     return result.fetchall()
     
