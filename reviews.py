52fa41a0137874c1329bf9cbff45a944353b74e1
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-23 16:32:18 +0300

    arviontia4

--- reviews.py
+++ reviews.py
@@ -7,7 +7,8 @@ def save_review(book_id, stars, text_review, visibility):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = "INSERT INTO reviews (book_id, stars, text_review, visibility, sent_at, user_id) VALUES (:book_id, :stars, :text_review, :visibility, :NOW(), :user_id)"
+    sql = "INSERT INTO reviews (book_id, stars, text_review, visibility, sent_at, user_id) VALUES (:book_id, :stars, :text_review, :visibility, NOW(), :user_id)"
     db.session.execute(sql, {"book_id":book_id, "stars":stars, "text_review":text_review, "visibility":visibility, "user_id":user_id})
     db.session.commit()
     return True
+
