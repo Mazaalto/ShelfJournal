66da5473c7c1e20bbf97d14596cfc0d55baed932
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-23 09:33:25 +0300

    Created myshelf page correction3

--- books.py
+++ books.py
@@ -11,7 +11,7 @@ def get_books_as_a_list():
 def get_my_books_as_a_list():
     # Toteutan tähän hakutoiminnallisuuden, missä haetaan vain omat kirjat 
     id = 1
-    sql = "SELECT B.id, B.book_title, B.author_name, B.sent_at, B.user_id FROM books B WHERE B.user_id=id ORDER BY B.book_title"
+    sql = "SELECT B.id, B.book_title, B.author_name, B.sent_at, B.user_id FROM books B WHERE B.user_id='id' ORDER BY B.book_title"
     result = db.session.execute(sql)
     return result.fetchall()
 
