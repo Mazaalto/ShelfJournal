bb7329eaa8781c406d6731214e380d9009f8e51a
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2021-04-23 08:48:10 +0300

    Created myshelf page

--- routes.py
+++ routes.py
@@ -35,6 +35,13 @@ def index():
     list = books.get_books_as_a_list()
     return render_template("index.html", count=len(list), books=list)
 
+@app.route("/myshelf")
+def myshelf():
+    # palauttaa kaikki kirjat, jotka ovat käyttäjän omia
+    id = session["user_id"]
+    list = get_my_books_as_a_list(id)
+    return render_template("myshelf.html", count=len(list), books=list)
+
 #@app.route("/mybooks", methods=["GET"])
 #def index2():
 #    id = user_id = request.form["id"]
